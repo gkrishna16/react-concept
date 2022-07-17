a56d7f95fa9880de8295ca0135f8251fc023f889
Gopal Krishan <70444883+gkrishna16@users.noreply.github.com>
2022-07-17 17:10:43 -0400

    added accordian code

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Searchinput } from './components/Searchinput';
-
+import React from "react";
+import "./App.css";
+import { Searchinput } from "./components/Searchinput";
+import AccoridianPage from "./components/AccoridianPage";
+const text = (
+  <p>
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+    Lorem Ipsum has been the industry's standard dummy text ever since the
+    1500s, when an unknown printer took a galley of type and scrambled it to
+    make a type specimen book. It has survived not only five centuries, but also
+    the leap into electronic typesetting.
+  </p>
+);
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <div>HOME PAGE</div>
-      <Searchinput/>
+      <Searchinput />
+      <AccoridianPage text={text} />
+      <AccoridianPage text={text} />
     </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
--- src/components/AccoridianPage.tsx
+++ src/components/AccoridianPage.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import "../App.css";
 
-const AccoridianPage = () => {
+const AccoridianPage = (props: any) => {
+  const [show, setShow] = useState<boolean>(false);
+
   return (
     <div>
-          <div className="accordian">
-              
+      <div className="accordian">
+        <div className="open" onClick={() => setShow(!show)}>
+          {show ? "Click to close" : "Click to open"}
+        </div>
+
+        <div className="">{show ? <p>{props.text}</p> : ""}</div>
       </div>
     </div>
   );
